5b33bb4e0a5d3c43b99b750a74f28fe39953dd03
Marina <6172331@mail.ru>
2022-08-25 23:56:15 +0300

    refactor: refactor project structure

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
 import './styles/index.scss'
 import './js/modal'
+
+function readNumber() {
+    let num;
+    while (true){
+        num = prompt('Input a number', '0');
+        if (!isFinite(num)){
+            break;
+        }
+        if (num === '' || num === null) break
+    }
+
+    return +num
+}
